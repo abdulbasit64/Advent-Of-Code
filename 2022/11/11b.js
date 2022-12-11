7daa055996c99a21df4b0c5f41862edffdf884e6
Abdul Basit <69422801+abdulbasit64@users.noreply.github.com>
2022-12-11 17:55:31 +0500

    Day 11 🐒

---
+++ 2022/11/11b.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+let input = fs.readFileSync('./input.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  return data;
+});
+input = input.split(/\r?\n/)
+let monkeys = {
+  // itemsInspected: 0
+}
+
+for (let i = 0; i < input.length; i++) {
+  let isMonkey = input[i].slice(0, 6) == 'Monkey'
+  let monkeyNo = input[i].slice(7, 8)
+  if (isMonkey){
+    monkeys[monkeyNo] = {
+      itemsInspected: 0,
+      startingItems: (input[++i].slice(18).split(', ')).map(a => parseInt(a)),
+      worryLevel: input[++i].slice(23).split(' '),
+      divideBy: [parseInt(input[++i].slice(21)), input[++i].split('throw to monkey ')[1], input[++i].split('throw to monkey ')[1]],
+    }
+  }
+}
+
+function worryValue(item, operator, value) {
+  return eval(item+operator+ (value=='old' ? item : value))
+}
+
+const mod = Object.keys(monkeys).map(a => {
+	return monkeys[a].divideBy[0]
+}).reduce((a,b)=> a*b)
+
+for (let k = 0; k < 10000; k++) {
+  for (let i = 0; i < Object.keys(monkeys).length; i++) {
+  let innerLoopLength = monkeys[i].startingItems.length
+  for (let j = 0; j < innerLoopLength; j++) {
+    monkeys[i].itemsInspected++;
+    const item = monkeys[i].startingItems.shift();
+    // Operation
+    let worryLevel = worryValue(item, monkeys[i].worryLevel[0], monkeys[i].worryLevel[1])
+    if (worryLevel % mod == 0)
+      monkeys[monkeys[i].divideBy[1]].startingItems.push(worryLevel)
+      else
+      monkeys[monkeys[i].divideBy[2]].startingItems.push(worryLevel)
+    }
+  }
+}
+let inspections = []
+Object.keys(monkeys).forEach(a =>{
+  inspections.push(monkeys[a].itemsInspected);
+})
+let top2MonkeyBusiness = inspections.sort((x, y) => y - x).slice(0, 2).reduce( (a,b) => a * b )
+// console.log("Inspections by each monkey:",inspections)
+console.log('Part 2:',top2MonkeyBusiness)
+
+
